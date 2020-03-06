918e48d470ffd467aa5adb794569d2085515ea6e
David J. Sandoval <davidjon2009@hotmail.com>
2020-03-06 10:32:05 -0400

    Se corrigio el problema con el Bean ProductMediator

--- app/src/main/java/bo/edu/uagrm/soe/facturacionsoe/FacturacionSoeApplication.java
+++ app/src/main/java/bo/edu/uagrm/soe/facturacionsoe/FacturacionSoeApplication.java
@@ -1,15 +1,14 @@
 package bo.edu.uagrm.soe.facturacionsoe;
 
-import bo.edu.uagrm.soe.facturacionsoe.config.ProductsConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 //@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class FacturacionSoeApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(FacturacionSoeApplication.class, args);
     }
+
 }
--- config/src/main/java/bo/edu/uagrm/soe/facturacionsoe/config/ProductsConfiguration.java
+++ config/src/main/java/bo/edu/uagrm/soe/facturacionsoe/config/ProductsConfiguration.java
@@ -1,6 +1,6 @@
 package bo.edu.uagrm.soe.facturacionsoe.config;
 
-import bo.edu.uagrm.soe.facturacionsoe.adapters.repositories.usecase.ProductRepositoryImpl;
+import bo.edu.uagrm.soe.facturacionsoe.usecases.ports.repositories.ProductRepository;
 import bo.edu.uagrm.soe.facturacionsoe.usecases.products.ProductMediator;
 import bo.edu.uagrm.soe.facturacionsoe.usecases.products.ProductMediatorImpl;
 import bo.edu.uagrm.soe.facturacionsoe.usecases.products.getall.GetAllProductsHandler;
@@ -9,13 +9,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ProductsConfiguration {
+
     @Bean("ProductMediator")
-    public ProductMediator getGetAllProductsHandler(GetAllProductsHandler getAllProductsHandler) {
-        return new ProductMediatorImpl(getAllProductsHandler);
+    public ProductMediator getGetAllProductsHandler(ProductRepository productRepository) {
+        return new ProductMediatorImpl(new GetAllProductsHandler(productRepository));
     }
 
-    @Bean
-    public GetAllProductsHandler getGetAllProductsHandler(ProductRepositoryImpl productRepository) {
-        return new GetAllProductsHandler(productRepository);
-    }
 }
